09ae8ed7d8c179e3020cd925a8022bbd8bde9239
Administrator <1783157378@qq.com>
2020-04-17 16:08:20 +0800

    太多了

--- src/main/java/com/bigdata/HelloA.java
+++ src/main/java/com/bigdata/HelloA.java
@@ -4,16 +4,5 @@ public class HelloA {
 	public static void main(String[] args) {
 		System.out.println("¹þ¹þ¹þ");
 
-		System.out.println("yyyy");
-
-		System.out.println("hhhh");
-
-		System.out.println("BBBB");
-
-		System.out.println("DDDD");
-
-		System.out.println("CCC");
-	
-
 	}
 }
